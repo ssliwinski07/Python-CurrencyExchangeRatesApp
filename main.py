5fc7e508de5a87a7e68cd87672b178e3c478638c
Szymon Śliwiński <122596933+ssliwinski07@users.noreply.github.com>
2024-07-16 22:11:19 +0200

    added loop for fetching data from api

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import asyncio
 from typing import List
+import time
 
 
-from utils.models.exchange_rates_model import ExchangeRatesModel
-from utils.services_http.exchange_rates_service_http import ExchangeRatesServiceHttp
-from utils.models.people_model import PeopleModel
-from utils.services_http.people_service_http import PeopleServiceHttp
+from lib.models.exchange_rates_model import ExchangeRatesModel
+from lib.services_http.exchange_rates_service_http import ExchangeRatesServiceHttp
+from lib.models.people_model import PeopleModel
+from lib.services_http.people_service_http import PeopleServiceHttp
+from lib.utils.utilities import Utilities
 
 
 async def main():
 
     exchange_rates_service: ExchangeRatesServiceHttp = ExchangeRatesServiceHttp()
-    people_service: PeopleServiceHttp = PeopleServiceHttp()
 
-    endpoint_nbp: str = "/exchangerates/tables/a/2029-06-05/?format=json"
-    endpoint_swapi: str = "/people/1/"
+    notes_dict = {}
+    notes_list: List[dict] = []
 
-    people: PeopleModel = await people_service.fetch_data(endpoint=endpoint_swapi)
+    year: int = 2024
+    month: int = 7
 
-    print(people.name)
+    days: int = Utilities.get_month_days(year, month)
+
+    for i in range(1, days + 1):
+        time.sleep(0.25)
+        year: str = "2024"
+        month: str = f"{month:02}"
+        days: str = f"{i:02}"
+        endpoint_nbp: str = (
+            f"/exchangerates/tables/a/{year}-{month}-{days}/?format=json"
+        )
+        exchange_rates = await exchange_rates_service.fetch_data(endpoint=endpoint_nbp)
+        if exchange_rates:
+            rates = exchange_rates[0].rates
+            notes_dict["effectiveDate"] = exchange_rates[0].effective_date
+            for rate in rates:
+                if rate.code == "CHF":
+                    notes_dict.update(dict(rate))
+            print(notes_dict)
 
 
 if __name__ == "__main__":
--- utils/consts/consts.py
+++
@@ -1,8 +0,0 @@
-# APIS
-BASE_API_NBP = "http://api.nbp.pl/api"
-BASE_API_SWAPI = "https://swapi.dev/api"
-
-### ALIASES FOR MODELS
-
-## ExchangeRatesModel
-EFFECTIVE_DATE = "effectiveDate"
