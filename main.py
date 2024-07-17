4c783e75d34bc5d9dada0f14f4799aa52e1dfbc0
Szymon Śliwiński <122596933+ssliwinski07@users.noreply.github.com>
2024-07-17 15:15:38 +0200

    added user's input

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from lib.utils.currencies.currencies_rates import CurrenciesRates
 
 
 async def main():
-    currencies_rates: List[dict] = (
-        await CurrenciesRates.fetch_currencies_rates_for_month(
-            year=2024, month=6, rates_to_look=["CHF", "USD"]
-        )
-    )
 
-    print(json.dumps(currencies_rates, indent=4))
+    while True:
+        month_input: int = int(input("Enter month: "))
+        year_input: int = int(input("Enter year: "))
+        currencies_rates: List[dict] = (
+            await CurrenciesRates.fetch_currencies_rates_for_month(
+                year=year_input, month=month_input, rates_to_look=["CHF", "USD"]
+            )
+        )
+        print(json.dumps(currencies_rates, indent=4))
+        break
 
 
 if __name__ == "__main__":
