2ab61528106959c9cdb4c9f8383c9084195ea5e6
Szymon Śliwiński <122596933+ssliwinski07@users.noreply.github.com>
2024-07-17 15:09:23 +0200

    added function, that fetches rates for whole month

--- main.py
+++ main.py
@@ -1,43 +1,18 @@
 import asyncio
+import json
 from typing import List
-import time
 
-
-from lib.models.exchange_rates_model import ExchangeRatesModel
-from lib.services_http.exchange_rates_service_http import ExchangeRatesServiceHttp
-from lib.models.people_model import PeopleModel
-from lib.services_http.people_service_http import PeopleServiceHttp
-from lib.utils.utilities import Utilities
+from lib.utils.currencies.currencies_rates import CurrenciesRates
 
 
 async def main():
-
-    exchange_rates_service: ExchangeRatesServiceHttp = ExchangeRatesServiceHttp()
-
-    notes_dict = {}
-    notes_list: List[dict] = []
-
-    year: int = 2024
-    month: int = 7
-
-    days: int = Utilities.get_month_days(year, month)
-
-    for i in range(1, days + 1):
-        time.sleep(0.25)
-        year: str = "2024"
-        month: str = f"{month:02}"
-        days: str = f"{i:02}"
-        endpoint_nbp: str = (
-            f"/exchangerates/tables/a/{year}-{month}-{days}/?format=json"
+    currencies_rates: List[dict] = (
+        await CurrenciesRates.fetch_currencies_rates_for_month(
+            year=2024, month=6, rates_to_look=["CHF", "USD"]
         )
-        exchange_rates = await exchange_rates_service.fetch_data(endpoint=endpoint_nbp)
-        if exchange_rates:
-            rates = exchange_rates[0].rates
-            notes_dict["effectiveDate"] = exchange_rates[0].effective_date
-            for rate in rates:
-                if rate.code == "CHF":
-                    notes_dict.update(dict(rate))
-            print(notes_dict)
+    )
+
+    print(json.dumps(currencies_rates, indent=4))
 
 
 if __name__ == "__main__":
